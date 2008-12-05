94334b329b1565213be5cd8c3c4ae65ebe71653e
Nick <nicholasjon@nicholasjon.com>
2008-12-05 13:06:25 -0600

    cleaned up comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ActiveRecord::Base
   include Authentication::ByPassword
   include Authentication::ByCookieToken
 
-  # A very important comment.
-  # Test
-
   has_many :feeds, :through => :feed_users
   has_many :clicks
   has_many :reads
@@ -25,14 +22,10 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  
-
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
   attr_accessible :login, :email, :name, :password, :password_confirmation, :identity_url
-
-
 
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
   #

b4e7fe8e47e3a276e01263c87a81c71a8e98daff
Nick <nicholasjon@nicholasjon.com>
2008-12-05 12:47:52 -0600

    fixed user unit test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -49,58 +49,56 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_should_reset_password
-    # kmb: broken
-    # users(:quentin).update_attributes(:password => 'new password', :password_confirmation => 'new password')
-    # assert_equal users(:quentin), User.authenticate('quentin', 'new password')
+    users(:keith).update_attributes(:password => 'new password', :password_confirmation => 'new password')
+    assert_equal users(:keith), User.authenticate('keith', 'new password')
   end
 
   def test_should_not_rehash_password
-    users(:quentin).update_attributes(:login => 'quentin2')
-    assert_equal users(:quentin), User.authenticate('quentin2', 'monkey')
+    users(:keith).update_attributes(:login => 'keith2')
+    assert_equal users(:keith), User.authenticate('keith2', 'monkey')
   end
 
   def test_should_authenticate_user
-    # kmb: broken
-    # assert_equal users(:quentin), User.authenticate('quentin', 'monkey')
+    assert_equal users(:keith), User.authenticate('keith', 'monkey')
   end
 
   def test_should_set_remember_token
-    users(:quentin).remember_me
-    assert_not_nil users(:quentin).remember_token
-    assert_not_nil users(:quentin).remember_token_expires_at
+    users(:keith).remember_me
+    assert_not_nil users(:keith).remember_token
+    assert_not_nil users(:keith).remember_token_expires_at
   end
 
   def test_should_unset_remember_token
-    users(:quentin).remember_me
-    assert_not_nil users(:quentin).remember_token
-    users(:quentin).forget_me
-    assert_nil users(:quentin).remember_token
+    users(:keith).remember_me
+    assert_not_nil users(:keith).remember_token
+    users(:keith).forget_me
+    assert_nil users(:keith).remember_token
   end
 
   def test_should_remember_me_for_one_week
     before = 1.week.from_now.utc
-    users(:quentin).remember_me_for 1.week
+    users(:keith).remember_me_for 1.week
     after = 1.week.from_now.utc
-    assert_not_nil users(:quentin).remember_token
-    assert_not_nil users(:quentin).remember_token_expires_at
-    assert users(:quentin).remember_token_expires_at.between?(before, after)
+    assert_not_nil users(:keith).remember_token
+    assert_not_nil users(:keith).remember_token_expires_at
+    assert users(:keith).remember_token_expires_at.between?(before, after)
   end
 
   def test_should_remember_me_until_one_week
     time = 1.week.from_now.utc
-    users(:quentin).remember_me_until time
-    assert_not_nil users(:quentin).remember_token
-    assert_not_nil users(:quentin).remember_token_expires_at
-    assert_equal users(:quentin).remember_token_expires_at, time
+    users(:keith).remember_me_until time
+    assert_not_nil users(:keith).remember_token
+    assert_not_nil users(:keith).remember_token_expires_at
+    assert_equal users(:keith).remember_token_expires_at, time
   end
 
   def test_should_remember_me_default_two_weeks
     before = 2.weeks.from_now.utc
-    users(:quentin).remember_me
+    users(:keith).remember_me
     after = 2.weeks.from_now.utc
-    assert_not_nil users(:quentin).remember_token
-    assert_not_nil users(:quentin).remember_token_expires_at
-    assert users(:quentin).remember_token_expires_at.between?(before, after)
+    assert_not_nil users(:keith).remember_token
+    assert_not_nil users(:keith).remember_token_expires_at
+    assert users(:keith).remember_token_expires_at.between?(before, after)
   end
 
 protected

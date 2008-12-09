a7ea615eed35fbaf4f0b663873fba92ee6c4caa9
Keith Burke <keith@brightshiny.me>
2008-12-09 17:38:39 -0600

    new gem

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "ezcrypto", :lit => "ezcrypto"
+  config.gem "simple-rss"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.

2d11a476b2d558cc06196f6fe5f00bbf75e6f063
Nick <nicholasjon@nicholasjon.com>
2008-12-15 10:12:52 -0600

    switched to using finder syntax"

--- app/controllers/list_of_items_controller.rb
+++ app/controllers/list_of_items_controller.rb
@@ -10,7 +10,14 @@ class ListOfItemsController < ApplicationController
       encrypted_user_id = KEY.url_safe_encrypt64(current_user.id)
       user_id = current_user.id
     end
-    @items = Item.find_by_sql "SELECT i.* from items i LEFT JOIN feed_users fu ON (fu.feed_id = i.feed_id) WHERE fu.user_id=#{user_id} ORDER BY i.published_at DESC LIMIT 50"
+    # @items = Item.find_by_sql "SELECT i.* from items i LEFT JOIN feed_users fu ON (fu.feed_id = i.feed_id) WHERE fu.user_id=#{user_id} ORDER BY i.published_at DESC LIMIT 50"
+    @items = Item.find(:all, 
+      :conditions => ["users.id = ?", user_id], 
+      :joins => "join feeds on (feeds.id = items.feed_id) join feed_users on (feeds.id = feed_users.feed_id) join users on (users.id = feed_users.user_id)", 
+      :include => "feed",
+      :limit => 50, 
+      :order => "published_at desc"
+    )
     @items.map { |item|
       encrypted_item_id = KEY.url_safe_encrypt64(item.id)
       item.content += " <img src=\"#{url_for(:controller => :reads, :action => :create, :u => encrypted_user_id, :i => encrypted_item_id)}\" alt=\"Whiskey Tracking\" /> "

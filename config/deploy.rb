df1103d8f5b5eedd44900e4eae7cb2aaed634282
Nicholas Olejniczak <nick@vroom.local>
2008-12-15 20:59:31 -0600

    organized the deploy script a little bit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,39 @@
 require 'net/ssh/proxy/http'
+
+# Configure this appropriately for your present local location
 behind_firewall = false # set me dynamically in the future!
 if behind_firewall
   proxy = Net::SSH::Proxy::HTTP.new( "itgproxy.redmond.corp.microsoft.com", 80 )
   ssh_options[:proxy] = proxy
 end
 
+# Remote server / file info 
 set :application, "whiskey.brightshiny.me"
-set :repository,  "ssh://brightshiny.me:443/home/whiskey/git/whiskey.git"
-set :user, "nick"
-set :use_sudo, true
-
-set :keep_releases, 3
-
+set :deploy_to, "/var/www/#{application}"
 ssh_options[:port] = 443
 ssh_options[:forward_agent] = true 
-default_run_options[:pty] = true 
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/var/www/#{application}"
+# Should we use "whiskey" + ssh_keys?  thoughts?
+set :user, "nick" 
+set :use_sudo, true
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
+# We're using git and we want to deploy using the remote cache (to prevent .git dirs / etc)
 set :scm, :git
 set :deploy_via, :remote_cache
+set :repository,  "ssh://brightshiny.me:443/home/whiskey/git/whiskey.git"
 
+# Number of releases to leave on server including the currently live version
+set :keep_releases, 3
+
+# Forces more passwords to be entered but prevents git from mistaking your account
+default_run_options[:pty] = true 
+
+# The location of our 3 servers
 role :app, application
 role :web, application
 role :db,  application, :primary => true
 
+# An extra task for restarting Phusion-based apps (though I don't think it works)
 namespace :deploy do
   desc "Restart Application"
   task :restart, :roles => :app do

173de020981d35aef3ab697b23461d1a61e0d068
Nick <nicholasjon@nicholasjon.com>
2008-12-17 10:27:08 -0600

    made the number of items in the sent feed a parameter

--- app/controllers/list_of_items_controller.rb
+++ app/controllers/list_of_items_controller.rb
@@ -22,11 +22,15 @@ class ListOfItemsController < ApplicationController
   end
   
   def index
+    num_items_to_send = 50
+    if ! params[:n].nil? && params[:n].to_i != 0
+      num_items_to_send = params[:n]
+    end
     @items = Item.find(:all, 
       :conditions => ["users.id = ?", @user.id], 
       :joins => "join feeds on (feeds.id = items.feed_id) join feed_users on (feeds.id = feed_users.feed_id) join users on (users.id = feed_users.user_id)", 
       :include => "feed",
-      :limit => 500, 
+      :limit => num_items_to_send, 
       :order => "published_at desc"
     )
     add_tracking_to_items(@items, @user.encrypted_id)

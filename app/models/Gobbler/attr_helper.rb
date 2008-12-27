74ea94880008d493278bfcc61546f91181e186d8
Keith Burke <keith@brightshiny.me>
2008-12-27 10:40:24 -0600

    class packaging re-arranged

--- app/models/Gobbler/attr_helper.rb
+++ app/models/Gobbler/attr_helper.rb
@@ -1,13 +1,15 @@
-class Gobbler::AttrHelper
-
-  def self.get_first(obj, attrs)
-    return nil if obj.nil? || attrs.nil?
+class Gobbler
+  class AttrHelper
     
-    attrs.each do |attr|
-      val = obj.send(attr)
-      return val if !val.nil? && val.to_s.length > 0
+    def self.get_first(obj, attrs)
+      return nil if obj.nil? || attrs.nil?
+      
+      attrs.each do |attr|
+        val = obj.send(attr)
+        return val if !val.nil? && val.to_s.length > 0
+      end
+      return nil
     end
-    return nil
+    
   end
-
 end

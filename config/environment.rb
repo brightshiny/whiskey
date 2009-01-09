5aa74793ebf71bc6ad3703d7357b86bed3a6f49f
Nicholas Olejniczak <nick@vroom.local>
2009-01-08 21:24:26 -0600

    required linalg

--- config/environment.rb
+++ config/environment.rb
@@ -88,8 +88,7 @@ require 'smtp_tls'
 require 'ezcrypto_url_safe'
 KEY = EzCrypto::Key.with_password "4e3064a5e2e0037812e6b7103fd73091dc29ac7e2cbb3e9a9e3092c4ac48603dd6ff0532bd12bc4ffcf95b36a7f6e7d88633073b28c717ee32c4e59e637dfe1a", "d995ca6318a590fd14df40311ed1b42ef7d96a67548c990e53b77752cd59f4306ee592e0b7010b68229db7fd388250b489111ad90a8e87d14b40576f7625c796", :algorithm=>"aes256"
 
-
-require 'smtp_tls' 
+require 'linalg'
 
 # Should probably find a better place for these...
 ActionMailer::Base.delivery_method = :smtp

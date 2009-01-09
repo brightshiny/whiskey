bd90663af11566f5b8b0ec9ccc32207ce04f6646
Nicholas Olejniczak <nick@vroom.local>
2009-01-08 21:23:27 -0600

    Don't need these anymore

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,6 @@ Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem "htmlentities"
   config.gem "stemmer"
-  config.gem "classifier"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.

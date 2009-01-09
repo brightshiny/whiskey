a1e486d1a5ab90e6e1ab86e238aa56ca58a56c26
Nicholas Olejniczak <nick@vroom.local>
2009-01-08 20:47:40 -0600

    made dimensionality reduction less manual

--- test/other/linalg_test.rb
+++ test/other/linalg_test.rb
@@ -41,3 +41,32 @@ puts
 puts "Reconstituting A: "
 a2 = u * s * vt
 puts a2
+
+k = 2 # dimensionality reduction
+cols_for_u2 = []
+cols_for_v2 = []
+eigenvectors = []
+
+k.times do |n|
+  cols_for_u2.push(u.column(n))
+  cols_for_v2.push(vt.column(n))
+  eigenvectors.push(s.column(n).to_a.flatten[0,k])
+end
+
+u2 = Linalg::DMatrix.join_columns cols_for_u2
+v2 = Linalg::DMatrix.join_columns cols_for_v2
+eig2 = Linalg::DMatrix.columns eigenvectors
+
+q = Linalg::DMatrix[[0,0,0,0,0,1,0,0,0,1,1]]
+qEmbed = q * u2 * eig2.inverse
+
+similarity_matrix, count = {}, 1
+v2.rows.each { |x|
+  similarity_matrix[count] = (qEmbed.transpose.dot(x.transpose)) / (x.norm * qEmbed.norm)
+  count += 1
+}
+
+puts
+
+similarity_matrix.each { |u| printf "ID: %d, Similarity: %0.3f \n", u[0], u[1]  }
+

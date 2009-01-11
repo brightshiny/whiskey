b3a43f3f0b3a07acb8ae1be2f0214ae1fa0bfdae
Nicholas Olejniczak <nick@vroom.local>
2009-01-11 10:06:08 -0600

    fixed bug in surgeon general where it didn't get a new decider per batch causing us to end up with ever-growing, very large deciders which was the thing we were trying to avoid in the first place

--- app/models/Classy/surgeon_general.rb
+++ app/models/Classy/surgeon_general.rb
@@ -5,8 +5,6 @@ module Classy
       
       u = User.find(3)
       
-      decider = Classy::Decider.new
-
       date_containing_items_consumed_data = Date.parse('2008-12-23')
       date_to_make_predictions_about = Date.parse('2008-12-24')
       
@@ -20,10 +18,13 @@ module Classy
             
       total_actually_consumed_docs = u.items_read_on(date_to_make_predictions_about)
       
-      batch_size = 20
+      batch_size = 100
       batch = 1
       while documents_to_make_prediction_from.size > 0
         
+        decider = Classy::Decider.new
+        
+        start_time = Time.now
         documents_to_make_prediction_from_this_batch = documents_to_make_prediction_from.shift(batch_size)
         
         puts "Processing batch ##{batch} (items #{(batch-1)*batch_size+1} - #{batch*batch_size})"
@@ -34,13 +35,13 @@ module Classy
       
         predicted_docs = decider.process_q(consumed_docs)
 
+        puts "\tDocuments in batch:\t\t#{documents_to_make_prediction_from_this_batch.size}"
         puts "\tSuggested documents:\t\t#{predicted_docs.size}"
 
         actual_item_ids = actually_consumed_docs.map{ |i| i.id }
 
         suggested_and_read = 0
         suggested_and_not_read = 0
-      
         predicted_and_read = 0
         predicted_and_not_read = 0
         predicted_docs.each do |predicted_item|
@@ -50,17 +51,21 @@ module Classy
             predicted_and_not_read += 1
           end
         end
-
         not_predicted_and_read = actual_item_ids.size - predicted_and_read
 
         puts "\tSuggested and consumed:\t\t#{predicted_and_read}"
         puts "\tSuggested and not consumed:\t#{predicted_and_not_read}"
         puts "\tNot suggested and consumed:\t#{not_predicted_and_read}"
-        puts
+        
+        puts "\tTime: #{end_time - start_time} seconds"
         batch += 1
+        
+        puts
+        end_time = Time.now   
       end
       
     end
     
   end
 end
+

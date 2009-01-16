1437d4020f3b345e571cac12f23c6532d07e6f71
Keith Burke <keith@brightshiny.me>
2009-01-15 19:49:38 -0600

    9 gabillion pct speed improvements

--- app/models/Classy/matrix_builder.rb
+++ app/models/Classy/matrix_builder.rb
@@ -10,6 +10,7 @@ module Classy
       @max_doc_index = -1
       @term_index_hash = Hash.new
       @doc_index_hash = Hash.new
+      @doc_term_count_cache = Hash.new
       @idf_cache = nil
       @dirty = false
     end
@@ -18,19 +19,24 @@ module Classy
       @dirty = true
       self.cached_a_tf_idf = nil
       doc_idx = get_doc_index(doc.id)
-      @columns[doc_idx] = doc_column(doc)
+      @columns[doc_idx], @doc_term_count_cache[doc_idx] = doc_column(doc)
     end
     
     # returns a array (doc column) with term counts in positions consistent for this instance
     # create_new=true will add new terms to the known index
     def doc_column(doc, create_new=true)
       col = Array.new(@max_term_index+1, 0.0)
+      doc_term_count = 0.0
       doc.item_words.each do |iw|
         word = iw.word.word
         term_idx = get_term_index(word,create_new)
-        col[term_idx] = iw.count unless term_idx == :term_dne
+        if term_idx != :term_dne
+          count = iw.count.to_f
+          col[term_idx] = count
+          doc_term_count += count
+        end
       end
-      return col
+      return [col, doc_term_count]
     end
     
     # make @columns[][] a rectangle and zero out nils
@@ -38,15 +44,15 @@ module Classy
       if @dirty
         @idf_cache = Array.new(@max_term_index+1)
         for term_idx in 0 .. @max_term_index
-          docs_with_term_count = 0
+          docs_with_term_count = 0.0
           for doc_idx in 0 .. @columns.size-1
             if @columns[doc_idx][term_idx].nil?
-              @columns[doc_idx][term_idx] = 0
+              @columns[doc_idx][term_idx] = 0.0
             elsif @columns[doc_idx][term_idx] > 0
-              docs_with_term_count += 1
+              docs_with_term_count += 1.0
             end
           end
-          @idf_cache[term_idx] = Math.log(@columns.size.to_f/docs_with_term_count.to_f)
+          @idf_cache[term_idx] = Math.log(@columns.size.to_f/docs_with_term_count)
         end
         @dirty = false
       end
@@ -71,18 +77,14 @@ module Classy
     end
     
     def q_term_count(item)
-      return Linalg::DMatrix.rows([doc_column(item,false)])
+      col, doc_term_count = doc_column(item, false)
+      return Linalg::DMatrix.rows([col])
     end
     
     def q_tf_idf(item)
-      col = doc_column(item, false)
-      term_count = 0
-      col.each {|n| term_count += n}
+      col, term_count = doc_column(item, false)
       for term_idx in 0 .. @max_term_index
-        tf = 0
-        if term_count.to_f != 0
-          tf = col[term_idx].to_f / term_count.to_f
-        end
+        tf = term_count > 0.0 ? col[term_idx] / term_count : 0.0
         idf = @idf_cache[term_idx]
         col[term_idx] = tf*idf
       end
@@ -105,13 +107,9 @@ module Classy
           tf_idf_columns[doc_idx] = Array.new
           for term_idx in 0 .. @max_term_index
             #word = @term_index_hash.index(term_idx)
-            word_count = @columns[doc_idx][term_idx]
-            doc_word_count = 0
-            @columns[doc_idx].each {|n| doc_word_count += n}
-            tf = 0
-            if doc_word_count.to_f != 0
-              tf = word_count.to_f / doc_word_count.to_f
-            end
+            term_count = @columns[doc_idx][term_idx]
+            doc_term_count = @doc_term_count_cache[doc_idx]
+            tf = doc_term_count > 0.0 ? (term_count / doc_term_count) : 0.0
             idf = @idf_cache[term_idx]
             tf_idf_columns[doc_idx][term_idx] = tf*idf
           end

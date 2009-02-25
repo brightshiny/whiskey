822fa22481a91beba2871f48c98d79f0f3771f97
Nicholas Olejniczak <nick@vroom.local>
2009-02-25 07:50:55 -0600

    now that we link out, we should probably block new signups to keep curious referrer log browsers out of the site

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,15 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new
-
-    if @user.signup!(params)
-      @user.deliver_activation_instructions!
-      flash[:notice] = "Your account has been created. Please check your e-mail for your account activation instructions!"
-      redirect_to root_url
-    else
-      render :action => :new
-    end
+    # @user = User.new
+    # 
+    # if @user.signup!(params)
+    #   @user.deliver_activation_instructions!
+    #   flash[:notice] = "Your account has been created. Please check your e-mail for your account activation instructions!"
+    #   redirect_to root_url
+    # else
+    #   render :action => :new
+    # end
   end
   
   def show

d5a7f211b29beb0c1f3e42f38f316f298855ff0a
Nicholas Olejniczak <nick@vroom.local>
2009-02-28 00:00:37 -0600

    coloring, ordering, organizing...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
   def handle_meme_column_assignment
     
     current_number_of_columns = 0
-    current_offset = 0
   
     @memes.each_with_index do |m, c|
       
@@ -15,23 +14,15 @@ module ApplicationHelper
       else 
         m.is_alpha = false
       end
-      
       m.number_of_columns = m.z_score_strength.ceil * SiteController::COLUMN_ZOOM_FACTOR
-      
       current_number_of_columns += m.number_of_columns
-      
-      if current_number_of_columns >= (SiteController::MAX_NUMBER_OF_COLUMNS - current_offset) || ((! next_item.nil?)&&((current_number_of_columns + next_item.z_score_strength.ceil*SiteController::COLUMN_ZOOM_FACTOR) > SiteController::MAX_NUMBER_OF_COLUMNS))
-        m.number_of_columns += (SiteController::MAX_NUMBER_OF_COLUMNS - current_offset) - current_number_of_columns
+      if current_number_of_columns >= SiteController::MAX_NUMBER_OF_COLUMNS || ((! next_item.nil?)&&((current_number_of_columns + next_item.z_score_strength.ceil*SiteController::COLUMN_ZOOM_FACTOR) > SiteController::MAX_NUMBER_OF_COLUMNS))
+        m.number_of_columns += SiteController::MAX_NUMBER_OF_COLUMNS - current_number_of_columns
         m.break_afterwards = true
         current_number_of_columns = 0
       else
         m.break_afterwards = false
       end        
-      
-      if c == 1
-        current_offset = SiteController::COLUMN_ZOOM_FACTOR
-      end
-      
     end
 
   end

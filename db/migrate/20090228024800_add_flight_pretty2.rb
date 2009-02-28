040ed2ad0abe9a4ac9da95060113998dc07ea33b
Keith Burke <keith@brightshiny.me>
2009-02-28 11:30:27 -0600

    fixed self.down scenario

--- db/migrate/20090228024800_add_flight_pretty2.rb
+++ db/migrate/20090228024800_add_flight_pretty2.rb
@@ -5,5 +5,6 @@ class AddFlightPretty2 < ActiveRecord::Migration
 
   def self.down
     f = Flight.find(:first, :conditions => ["partial_name = ? and action_name = ? and controller_name = ?", "pretty2", "index", "site"])
+    Flight.delete(f.id) if f
   end
 end

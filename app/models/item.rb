120687dd9ab047f344d0c5f074ea413d2667a8b4
Keith Burke <keith@brightshiny.me>
2009-02-28 12:02:19 -0600

    renamed avgerage to avg to match db columns, streamlined total and avg cosine methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -41,16 +41,16 @@ class Item < ActiveRecord::Base
 
   def total_cosine_similarity(run)
     total_cosine_similarity = 0
-    item_relationships = ItemRelationship.find(:all, :conditions => ["item_id = ? and run_id = ?", self.id, run.id])
+    item_relationships = ItemRelationship.find(:all, :conditions => ["item_id = ? and run_id = ?", self.id, run.id], :select => "cosine_similarity")
     item_relationships.each { |ir| 
       total_cosine_similarity += ir.cosine_similarity
     }
     return total_cosine_similarity
   end
 
-  def average_cosine_similarity(run)
+  def avg_cosine_similarity(run)
     num_item_relationships_in_run = 0
-    item_relationships = ItemRelationship.find(:all, :conditions => ["item_id = ? and run_id = ?", self.id, run.id])
+    item_relationships = ItemRelationship.find(:all, :conditions => ["item_id = ? and run_id = ?", self.id, run.id], :select => "cosine_similarity")
     item_relationships.each { |ir| 
       num_item_relationships_in_run += 1
     }

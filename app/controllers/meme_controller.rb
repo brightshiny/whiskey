660a778e2a52e16ffa7cd108c57f6f48c2b31341
Keith Burke <keith@brightshiny.me>
2009-02-28 12:03:43 -0600

    updated run diagnostics to use meme_items total and avg cosine similarity columns

--- app/controllers/meme_controller.rb
+++ app/controllers/meme_controller.rb
@@ -3,7 +3,16 @@ class MemeController < ApplicationController
   before_filter :require_user, :except => :show
 
   def show
-    @meme = Meme.find(params[:id], :include => [:item_relationships => :item])
+    @meme = Meme.find(params[:id], :include => [ :meme_items => {:item_relationship => { :item => :feed} } ] )
+  
+    seen_meme_items = Hash.new
+    @distinct_meme_items = Array.new
+    @meme.meme_items.each do |mi|
+      item = mi.item_relationship.item
+      next if seen_meme_items.has_key?(item.id)
+      seen_meme_items[item.id] = true
+      @distinct_meme_items.push mi
+    end
     respond_to do |format|
       format.html
       format.xml

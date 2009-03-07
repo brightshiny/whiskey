be67a0186e3e75366a515cd4a0e93ae5b53c796c
Nicholas Olejniczak <nick@vroom.local>
2009-03-06 20:33:03 -0600

    added redcloth for text formatting

--- config/environment.rb
+++ config/environment.rb
@@ -32,6 +32,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic"
   config.gem "htmlentities"
   config.gem "stemmer"
+  config.gem "RedCloth"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.

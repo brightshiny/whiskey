182bcbe2203f8d7e4b82b215f6b28dc154bbe5df
Nicholas Olejniczak <nick@vroom.local>
2009-03-06 23:34:47 -0600

    worked on cleaning up page 2... for fun

---
+++ app/controllers/graph_controller.rb
@@ -0,0 +1,51 @@
+class GraphController < ApplicationController
+
+  def index
+    @graph = open_flash_chart_object(960,300,"/graph/meme_strength")
+  end
+
+  def meme_strength
+    @meme = Meme.find(params[:id])
+    
+    related_memes = @meme.related_memes.reverse
+
+    strengths = related_memes.map{ |m| m.strength }
+    
+    chart = OpenFlashChart.new
+    # title = Title.new("Strength")
+    # title.set_style( "{font-size: 20px; font-family: Helvetica, 'sans serif'; font-weight: bold; color: #333; text-align: center;}" );
+    # chart.set_title(title)
+
+    bar = Bar.new
+    bar.set_values(strengths)
+    bar.colour = '#D24A31'
+
+    chart.add_element(bar)
+
+    tooltip = Tooltip.new
+    tooltip.set_stroke(1)
+    tooltip.set_colour("#777777")
+    tooltip.set_text_color("#777777")
+    bar.set_tooltip("#val#")
+    
+    y = YAxis.new
+    y.set_range(0,strengths.max+10,10)
+    chart.set_y_axis(y)
+
+    x = XAxis.new
+    x_axis_labels = Array.new(strengths.size)
+    x_axis_labels[0] = related_memes.first.run.ended_at.strftime('%m/%d/%Y %I%p')
+    x_axis_labels[strengths.size-1] = related_memes.last.run.ended_at.strftime('%m/%d/%Y %I%p')
+    x.set_labels(x_axis_labels)
+    chart.set_x_axis(x)
+
+    chart.set_bg_colour('#ffffff')
+    x.set_grid_colour('#efefef')
+    x.set_colour('#777777')
+    y.set_grid_colour('#efefef')
+    y.set_colour('#777777')
+    
+    render :text => chart.to_s
+  end
+  
+end

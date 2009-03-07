684f41c7793d2f71244323a7ee2620be868199fd
Nicholas Olejniczak <nick@vroom.local>
2009-03-06 19:47:44 -0600

    clickable "update" link

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,8 +3,8 @@ function should_page_be_updated(responseText) {
     if(! $('time_to_update')) {
       s = document.createElement("span");
       s.id = "time_to_update";
-      s.innerHTML = " &larr; we've updated, refresh to get the latest edition";
       s.style.display = "none";
+      s.innerHTML = " <a href=\"/\">&larr; we've updated, refresh to get the latest edition</a>";
       logo = $('logo');
       logo.appendChild(s);
       Effect.toggle('time_to_update','appear');

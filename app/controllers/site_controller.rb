22ebf6c022d5abf71dc2db2834b7fa1d0c0e80fe
Nick <nicholasjon@nicholasjon.com>
2009-03-18 21:06:55 -0500

    added caching for meme pages

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -74,39 +74,41 @@ class SiteController < ApplicationController
     end
   end
 
-  def meme    
-    @meme = Meme.find(:first, :conditions => { :id => params[:id] }, :include => [ :meme_items => { :item_relationship => { :item => :feed } } ] )
-    potential_items = @meme.related_memes.map{ |m| m.distinct_meme_items }.flatten.map{ |mi| mi.item }.sort_by{ |i| i.published_at }.reverse
-    items_hash = {}
-    potential_items.each { |i|
-      items_hash[i.id] = i
-    }
-    @items = []
-    items_hash.keys.each { |k|
-      @items.push(items_hash[k])
-    }
-    @words = Word.find_by_sql(["select w.id, w.word, sum(iw.count) as number_of_occurances from memes m join meme_items mi on mi.meme_id = m.id join item_relationships ir on ir.id = mi.item_relationship_id join item_words iw on iw.item_id = ir.item_id join words w on w.id = iw.word_id where m.id = ? group by w.id order by 3 desc limit 10", @meme.id])
-    @words_for_twitter_search = []
-    number_of_words_for_twitter_search = 2
-    @words[0..(number_of_words_for_twitter_search-1)].each { |partial_word| 
-      reg = Regexp.new("\s#{partial_word.word}.*?\s",true)
-      all_matching_words = Gobbler::GItem.extract_text(@meme.item.content).strip.scan(reg).map{ |s| s.strip }
-      matching_words = {}
-      all_matching_words.each { |word| 
-        word = word.gsub(/[a-z]s$/i,'')
-        if matching_words[word].nil?
-          matching_words[word] = 0
-        end
-        matching_words[word] += 1
+  def meme  
+    if ! read_fragment({ :action => "meme", :id => params[:id], :flight => @flight.id })      
+      @meme = Meme.find(:first, :conditions => { :id => params[:id] }, :include => [ :meme_items => { :item_relationship => { :item => :feed } } ] )
+      potential_items = @meme.related_memes.map{ |m| m.distinct_meme_items }.flatten.map{ |mi| mi.item }.sort_by{ |i| i.published_at }.reverse
+      items_hash = {}
+      potential_items.each { |i|
+        items_hash[i.id] = i
       }
-      matching_words = matching_words.sort { |a,b| b[1] <=> a[1] }
-      word = matching_words[0][0].gsub(/\W/,'').gsub(/\342\200\231s/,'')
-      @words_for_twitter_search.push(word)
-    }
+      @items = []
+      items_hash.keys.each { |k|
+        @items.push(items_hash[k])
+      }
+      @words = Word.find_by_sql(["select w.id, w.word, sum(iw.count) as number_of_occurances from memes m join meme_items mi on mi.meme_id = m.id join item_relationships ir on ir.id = mi.item_relationship_id join item_words iw on iw.item_id = ir.item_id join words w on w.id = iw.word_id where m.id = ? group by w.id order by 3 desc limit 10", @meme.id])
+      @words_for_twitter_search = []
+      number_of_words_for_twitter_search = 2
+      @words[0..(number_of_words_for_twitter_search-1)].each { |partial_word| 
+        reg = Regexp.new("\s#{partial_word.word}.*?\s",true)
+        all_matching_words = Gobbler::GItem.extract_text(@meme.item.content).strip.scan(reg).map{ |s| s.strip }
+        matching_words = {}
+        all_matching_words.each { |word| 
+          word = word.gsub(/[a-z]s$/i,'')
+          if matching_words[word].nil?
+            matching_words[word] = 0
+          end
+          matching_words[word] += 1
+        }
+        matching_words = matching_words.sort { |a,b| b[1] <=> a[1] }
+        word = matching_words[0][0].gsub(/\W/,'').gsub(/\342\200\231s/,'')
+        @words_for_twitter_search.push(word)
+      }
     
-    @page_title = "meme details for #{@meme.id}"
-    @meme_strength_graph = open_flash_chart_object(960,300,"/graph/meme_strength/#{@meme.id}")
-    @items_published_graph = open_flash_chart_object(960,100,"/graph/items_published/#{@meme.id}")
+      @page_title = "meme details for #{@meme.id}"
+      @meme_strength_graph = open_flash_chart_object(960,300,"/graph/meme_strength/#{@meme.id}")
+      @items_published_graph = open_flash_chart_object(960,100,"/graph/items_published/#{@meme.id}")
+    end
   end
   
 end

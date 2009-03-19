edca95953b7703f7c7a0a111283496b5579be475
Nick <nicholasjon@nicholasjon.com>
2009-03-18 22:11:05 -0500

    more word fixing

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -101,7 +101,7 @@ class SiteController < ApplicationController
           matching_words[word] += 1
         }
         matching_words = matching_words.sort { |a,b| b[1] <=> a[1] }
-        word = matching_words[0][0].gsub(/\W/,'').gsub(/\342\200\231s/,'')
+        word = matching_words[0][0].gsub(/\W/,'').gsub(/\342\200\231s/,'').gsub(/\342\200\235/,'')
         @words_for_twitter_search.push(word)
       }
     
